39eb3a63dde0020f34b2c6c8feec8cdaedb271a1
wotj7687 <48235807+wotj7687@users.noreply.github.com>
2019-04-07 21:38:31 +0900

    assignment for "2019/04/09"

--- client.py
+++ client.py
@@ -1,40 +1,21 @@
-import socket
+## client.py
 
+import socket
 import argparse
 
-
-
-def run(host, port, fi):
-
-
+def run(host, port):
     with socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM) as s:
-
-        s.connect((host, port))  ## 상대방 IP주소, 상대방 port 번호
-
-        s.sendall(fi.encode()) ##  string을 byte로 바꾸는 거
-
-        resp = s.recv(30)
         
-        filesize = int(resp.decode())
-
-        data = s.recv(filesize)
-
-        with open(fi, 'wb') as f:
-                f.write(data)
-        print("filesize : ", filesize)
+        s.connect((host, port))  ## 상대방 IP주소, 상대방 port 번호
+        line = input(':')
+        s.sendall(line.encode())
+        resp = s.recv(1024)
+        print(resp.decode())
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description="Echo client -p port -i host")
-
     parser.add_argument("-p", help = "port_number", required = True)
-
     parser.add_argument("-i", help="host_name", required=True)
 
-    parser.add_argument('-f', help="File_name", required = True)
-
-
-
     args = parser.parse_args()
-
-    run(host=args.i, port=int(args.p), fi = args.f)
+    run(host=args.i, port=int(args.p))
